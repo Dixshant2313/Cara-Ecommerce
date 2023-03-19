709a35a923f5af812e56f94cf03aa2061a18b8d9
Dixshant2313 <dixshantcharan2313@gmail.com>
2023-03-19 21:41:36 +0530

    modified:   index.html 	new file:   prodetail.html (added product details page) 	modified:   script.js 	new file:   shop.html (added shop page) 	modified:   style.css

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const close = document.getElementById('close');
+// Responsive Navbar Code
+
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
 if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 }
+
+// Product Detail Page Slider
+
+const mainImg = document.getElementById("MainImg");
+const smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = () => {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = () => {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = () => {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = () => {
+  mainImg.src = smallImg[3].src;
+};
